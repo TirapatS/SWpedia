2081cb3f29e84341e51ef8351af5e7bfe0c7e524
TirapatS <tirapat.s02@gmail.com>
2022-04-20 15:04:00 +0200

    Styled HomePage, added Navbar and empty style.css for future

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 
 import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
 import { Link } from 'react-router-dom'
 
 const Navigation = () => {
@@ -9,12 +8,6 @@ const Navigation = () => {
 		<Navbar bg="dark" variant="dark" expand="md">
 			<Container>
 				<Navbar.Brand as={Link} to="/"><h1>SWpedia</h1></Navbar.Brand>
-
-				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className="ms-auto">
-					</Nav>
-				</Navbar.Collapse>
 			</Container>
 		</Navbar>
 	)
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,46 @@
+import '../assets/style.css'
+
+// Images
+import MoviePoster from '../assets/images/sw-movie-poster.jpg'
+import PeoplePoster from '../assets/images/sw-characters-poster.jpg'
+
+
+// React Bootstrap imports
+import Image from 'react-bootstrap/Image'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+
+
 const HomePage = () => {
     return (
         <>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus ut, beatae quos mollitia maxime expedita cumque quod explicabo atque tenetur, aspernatur dolorem voluptatum illum corporis in quisquam soluta nam voluptas.</p>
+            <Container fluid="md">
+                <div className="choice-container">
+                    <div className="d-flex justify-content-center">
+                        <div className="mx-5 mt-5">
+                            <div>
+                                <Image src={MoviePoster} height={400}></Image>
+                            </div>
+                            <div className="mt-4 text-center">
+                                <Button variant="outline-light" size="md">
+                                    Movies
+                                </Button>
+                            </div>
+                        </div>
+
+                        <div className="choice-container mx-5 mt-5">
+                            <div className="mx-auto">
+                                <Image src={PeoplePoster} height={400}></Image>
+                            </div>
+                            <div className="mt-4 text-center">
+                                <Button variant="outline-light" size="md">
+                                    Peoples
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Container>
         </>
     )
 }
