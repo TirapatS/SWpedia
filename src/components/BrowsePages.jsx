09b7cf1da79438426cda05199143b5add043e4e7
TirapatS <tirapat.s02@gmail.com>
2022-04-21 10:20:24 +0200

    BrowsePage component is work in progress

--- src/components/BrowsePages.jsx
+++ src/components/BrowsePages.jsx
@@ -1,13 +1,14 @@
 import { Link } from 'react-router-dom'
 import Button from 'react-bootstrap/Button'
 
+// Work in progress
 const BrowsePage = () => {
 	return (
         <div className="mt-3">
-            <Button as={Link} to="/film" variant="secondary" size="md">
+            <Button as={Link} to="" variant="secondary" size="md">
                 Previous Page
             </Button>
-            <Button as={Link} to="/film" variant="secondary" size="md">
+            <Button as={Link} to="" variant="secondary" size="md">
                 Next Page
             </Button>
         </div>
